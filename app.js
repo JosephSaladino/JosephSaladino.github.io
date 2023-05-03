78444b3b5c5353ddbed6891be8069c6346202885
BoboRui <113151898+BoboRui@users.noreply.github.com>
2023-05-03 08:07:11 -0400

    scroll

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
     $('html, body').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
-    }, 10);
+    }, 500);
   });
+
+  function smoothScroll(target, duration) {
+    const targetPosition = target.offsetTop;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+  
+    function animation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+  
+    function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+  
+    requestAnimationFrame(animation);
+  }
+  
 
 // Toggle the mobile navigation menu
 $('.nav-toggle').click(function() {
