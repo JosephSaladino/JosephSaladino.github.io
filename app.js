bdcebecb9e6261238c64cda7dc67bafea68029be
BoboRui <113151898+BoboRui@users.noreply.github.com>
2023-05-04 07:23:35 -0400

    bounce

--- app.js
+++ app.js
@@ -66,5 +66,52 @@ window.addEventListener('resize', setBgHeight);
 
 setBgHeight();
 
+
+// Get the canvas element
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+// Set the initial position and velocity of the circle
+let x = canvas.width / 2;
+let y = canvas.height / 2;
+let dx = 2;
+let dy = -2;
+
+// Define the draw function
+function draw() {
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw the circle
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+
+  // Update the circle position
+  x += dx;
+  y += dy;
+
+  // Bounce the circle off the edges of the canvas
+  if (x + dx > canvas.width - 10 || x + dx < 10) {
+    dx = -dx;
+  }
+  if (y + dy > canvas.height - 10 || y + dy < 10) {
+    dy = -dy;
+  }
+}
+
+// Define the animate function
+function animate() {
+  // Draw the circle
+  draw();
+
+  // Call the animate function on the next animation frame
+  requestAnimationFrame(animate);
+}
+
+// Start the animation
+animate();
   
       
